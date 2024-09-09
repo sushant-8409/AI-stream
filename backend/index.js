1a49310680af12c44d238cec8119408f47c431bf
SUSHANT KUMAR SINGH <sushantkumarsingh8409@gmail.com>
2024-09-09 19:21:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -157,14 +157,14 @@ app.use((err, req, res, next) => {
 });
 
 // PRODUCTION
-app.use(express.static(path.join(__dirname, "../client/dist")));
-app.use(express.static(path.join(__dirname, "../client")));
+// app.use(express.static(path.join(__dirname, "../client/dist")));
+// app.use(express.static(path.join(__dirname, "../client")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
-  res.sendFile(path.join(__dirname, "../client", "index.html"));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+//   res.sendFile(path.join(__dirname, "../client", "index.html"));
 
-});
+// });
 
 app.listen(port, () => {
   connect();
