6a43573440875ee9c6ecba9787376ac2853957d2
SUSHANT KUMAR SINGH <sushantkumarsingh8409@gmail.com>
2024-09-12 01:42:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from "express";
-// import cors from "cors";
+import cors from "cors";
 import path from "path";
 import url, { fileURLToPath } from "url";
 import ImageKit from "imagekit";
