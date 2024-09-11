b4be079d52796b299de3a0f6c4cc998730a39e4b
SUSHANT KUMAR SINGH <sushantkumarsingh8409@gmail.com>
2024-09-12 01:09:20 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 5173,  // Use PORT environment variable if set, otherwise fallback to 5173
+    host: true  // This exposes the server to the network (necessary for Render)
+  }
 })
